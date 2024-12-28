f45b9ce5b09fe7a0aa5b98b37b5f625cd9606e77
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-12-28 10:53:59 +0100

    Change email as required field and usernamenot

--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,15 @@ from django.core.validators import RegexValidator
 
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=255, unique=True, validators=[RegexValidator(regex=r'^[\w.@+\-_\s]+$')])
+    username = models.CharField(max_length=255, unique=False, validators=[RegexValidator(regex=r'^[\w.@+\-_\s]+$')])
     email = models.EmailField(unique=True)
     uploaded_avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     selected_avatar = models.CharField(max_length=255, blank=True, null=True)
     is_terms_accepted = models.BooleanField(default=True)
     is_online = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] 
 
     def __str__(self):
         return self.email
